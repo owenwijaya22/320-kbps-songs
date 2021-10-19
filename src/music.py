a78efbdd3a9c146c7e021c43b467efa72dc7c403
owen wijaya <owenwijaya89@gmail.com>
2021-10-19 21:38:49 +0700

    song_program

---
+++ src/music.py
@@ -0,0 +1,21 @@
+import youtube_dl
+urls = []
+while True:
+    url = input('Url:')
+    if url == '':
+        break
+    else:
+        urls.append(url)
+print(urls)
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'noplaylist': 'true',
+    'outtmpl': 'G:/My Drive/320kbps_songs' + '/%(title)s.%(ext)s',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '320',
+    }],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(urls)
