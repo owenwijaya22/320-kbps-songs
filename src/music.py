5cd89f62d10eac00f2f779005950c8770c5fd77f
Owen Valentinus <owenwijaya89@gmail.com>
2022-01-10 08:16:15 +0700

    flac update

--- src/music.py
+++ src/music.py
@@ -11,11 +11,9 @@ ydl_opts = {
     'format': 'bestaudio/best',
     'noplaylist': 'true',
     'outtmpl': 'G:/My Drive/320kbps_songs' + '/%(title)s.%(ext)s',
-    #'outtmpl': ./320kbps_songs' + '/%(title)s.%(ext)s',
-    'postprocessors': [{
+        'postprocessors': [{
         'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '320',
+        'preferredcodec': 'flac',
     }],
 }
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
